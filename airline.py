6b6ba30b1b54b9c4f73058dde011fecf0dccda75
Jongkook Choi <jongkook90@gmail.com>
2023-10-04 17:58:07 +0900

    Initial commit

---
+++ airline.py
@@ -0,0 +1,231 @@
+import numpy as np 
+import scipy.stats as ss
+import pandas as pd
+import geopy.distance
+
+# def black_scholes(S,K,ttm,sigma):
+#     # S : current
+#     # K : strike
+#     sigma_tau = sigma * (ttm**0.5)
+#     d1 = ( np.log(S/K) + (0.5*(sigma**2)) * ttm ) / ( sigma_tau ) if S>0 else -np.inf
+#     d2 = d1 - sigma_tau
+#     return ss.norm.cdf(d1) * S - ss.norm.cdf(d2) * K
+#     #pd.DataFrame({ttm:{delay: bs(delay,15,ttm,0.1) for delay in np.arange(0,30,0.1)} for ttm in (1,10,20,30)}).plot()
+
+
+class Sky:
+    def __init__(self, df_time, timestep = 5, debug=False):
+        self.df_flight = pd.DataFrame({"org":[], "dst":[], "time_plan":[], "delay":[]})
+        self.df_time = df_time 
+#         self.balance = 0
+        self.time = 0
+        self.timestep = timestep
+        self.debug = debug
+
+    def update(self, flight_info, landed_idx):
+        self.df_flight = self.df_flight[~self.df_flight.index.isin(landed_idx)]
+        new_flights = pd.DataFrame([self._new_flight(info) for info in flight_info])
+        self.df_flight = pd.concat(
+            [self.df_flight, new_flights]
+        ).sort_values("time_plan").reset_index(drop=True)
+        if self.debug and (len(new_flights)>0):
+            print(new_flights)
+        self.time += self.timestep
+        return new_flights
+        
+    def _new_flight(self, flight_info):
+        origin, destination, delay = flight_info
+        fight_time = self.df_time.loc[(origin,destination)]
+        return {
+            "org":origin, "dst":destination, 
+            "time_plan":self.time+fight_time, 
+            "delay":delay}
+    
+    def add_random_flight(self, df_preference, n):
+        tmp = df_preference.stack()
+        pairs = np.random.choice(tmp.index, n, replace=False, p=tmp.values/tmp.sum())
+        self.update([(orig, dest, 0) for orig, dest in pairs], [])
+        
+    @property
+    def arrivals(self):
+        arrivals = self.df_flight[(self.df_flight["time_plan"]+self.df_flight["delay"])<=self.time]
+        return dict(list(arrivals.groupby("dst")))
+    
+
+class Airport:
+    def observe(self):
+        delays = [ min(60,max(-60, self.time - x["takeoff_plan"])) for x in self.arrivals ]
+        delays = sorted(delays, reverse=True)
+        return delays
+    
+    def __init__(self, df_preference, code, std_delay=5, late_threshold=15, holding_period=120, timestep=5, debug=False):
+        self.late_threshold = 15
+        self.holding_period = holding_period # minutes between landing and takeoff. (~TTM)
+        self.code = code
+        self.time = 0
+        self.timestep = timestep
+        self.plane_id = 0
+        pref = df_preference.loc[code]
+        self.p = pref / (pref.sum())
+        self.sigma = std_delay / late_threshold
+        self.std_delay = std_delay
+        # self.df_inventory = pd.DataFrame({"id":[], "dst":[], "time_plan":[], "time_est":[]})
+        self.arrivals = {}
+        self.debug=debug
+        self.policy_lst = [
+            lambda costs: min(costs,key=lambda x:costs[x]["landing_time"]), # FIFO
+            lambda costs: max(costs,key=lambda x:costs[x]["intrinsic_cost"]), # urgent first, based on arrival time
+            lambda costs: max(costs,key=lambda x:costs[x]["est_cost"] - costs[x]["intrinsic_cost"]), # urgent first, based on profit
+        ]
+        self.policy = 0
+        
+        self.accumulate_delay = True if code.startswith("RK") else False
+    
+    def calc_delay(self):
+        return np.random.normal(0,self.std_delay)
+    
+    def update(self, df_arrivals):
+        if df_arrivals is not None:
+            self._land(df_arrivals)
+        costs = {code: self._est_cost(code) for code,info in self.arrivals.items() if info["takeoff_ready"]<=self.time}
+        if self.debug:
+            print(f"{self.code} Costs : \n{costs}")
+        plane_id = self._select_plane(costs)
+        if plane_id is not None:
+            flight_info = self._takeoff(plane_id) #( self.code, next_station, delay )
+            cost = flight_info[2]
+        else:
+            flight_info = None
+            cost = None
+        # print(cost) # TODO
+        self.time += self.timestep
+        return flight_info
+        
+    def _select_plane(self, costs):
+        if len(costs)>0:
+            return self.policy_lst[self.policy](costs)
+        else:
+            return None
+   
+    def _land(self, df_arrivals):
+        arrivals = dict()
+        for _, info in df_arrivals.iterrows():
+            arrivals[self.plane_id] = {
+                "landing_plan":info["time_plan"],
+                "landing_time":self.time,
+                "takeoff_plan":info["time_plan"] + self.holding_period,
+                "takeoff_ready": (
+                    self.time + self.holding_period + self.calc_delay() if self.accumulate_delay
+                    else info["time_plan"] + self.holding_period + self.calc_delay()
+                ),
+                }
+            self.plane_id += 1
+        if self.debug:
+            debugstr = '\n'.join([str((k,v)) for k,v in arrivals.items()])
+            print(f"{self.code} Landing : \n{debugstr}")
+        self.arrivals.update(arrivals)
+        
+    def _est_cost(self, plane_id):
+        x = self.arrivals[plane_id]
+        orig_delay = x["landing_time"] - x["landing_plan"]
+        intrinsic_cost = max(0,orig_delay-self.late_threshold)
+        est_cost = self.black_scholes(orig_delay, self.late_threshold, 1, self.sigma)
+        return {"landing_time": x["landing_time"], "intrinsic_cost":intrinsic_cost, "est_cost":est_cost}
+        
+    def _takeoff(self, plane_id):
+        flight = self.arrivals.pop(plane_id)
+        assert(flight["takeoff_ready"]<=self.time)
+        next_station = np.random.choice(self.p.index, p=self.p.values)
+        delay = max(0,self.time-flight["takeoff_plan"])
+        info = ( self.code, next_station, delay )
+        if self.debug:
+            print(f"{self.code} Takeoff : plane_id: {plane_id}, info: {info}")
+        return info
+            
+    @staticmethod
+    def black_scholes(S,K,ttm,sigma):
+        # S : current
+        # K : strike
+        sigma_tau = sigma * (ttm**0.5)
+        d1 = ( np.log(S/K) + (0.5*(sigma**2)) * ttm ) / ( sigma_tau ) if S>0 else -np.inf
+        d2 = d1 - sigma_tau
+        return ss.norm.cdf(d1) * S - ss.norm.cdf(d2) * K
+        #pd.DataFrame({ttm:{delay: bs(delay,15,ttm,0.1) for delay in np.arange(0,30,0.1)} for ttm in (1,10,20,30)}).plot()
+
+def get_df():
+    import os.path
+    if os.path.isfile("df_preference.pkl") :
+        df_preference = pd.read_pickle("df_preference.pkl")
+        df_airports = pd.read_pickle("df_airports.pkl")
+    else:
+        df = pd.read_pickle("analysis.pkl")
+        df_preference_stack = df.groupby(["출발지","도착지"])["예상"].count()
+        df_preference = df_preference_stack.unstack().fillna(0)
+        df_loc = pd.read_csv("./kaggle/airports.csv")
+        airports = list(set(df_preference.columns).intersection(set(df_preference.index)).intersection(set(df_loc["ICAO"].values)))
+        df_preference = df_preference.loc[airports,airports]
+        df_airports = pd.Series(airports)
+        df_preference.to_pickle("df_preference.pkl")
+        df_airports.to_pickle("df_airports.pkl")
+
+    if os.path.isfile("df_time.pkl"):
+        df_time = pd.read_pickle("df_time.pkl")
+    else:
+        df_loc = pd.read_csv("./kaggle/airports.csv")
+        df_loc = df_loc.set_index("ICAO")[["Latitude","Longitude"]]
+        df_loc = df_loc.loc[list(set(df_loc.index).intersection(set(df_preference.columns).union(df_preference.index)))]
+        infer_time = lambda lat1, lon1, lat2, lon2 : geopy.distance.geodesic((lat1, lon1), (lat2,lon2)).km * 0.08 #(~750km/h)
+
+        result = dict()
+        for i1,x1 in df_loc.iterrows():
+            buf = dict()
+            for i2,x2 in df_loc.iterrows():
+                buf[i2] = infer_time(x1["Latitude"],x1["Longitude"],x2["Latitude"],x2["Longitude"])
+            result[i1] = buf
+        df_time = pd.DataFrame(result).stack()
+        df_time.to_pickle("df_time.pkl")
+    
+    return df_airports, df_preference, df_time
+    
+class Simulator:
+    def __init__(self, cfg, dfs=None, num_plane=100):
+        if dfs is None:
+            df_airline, df_preference, df_time = get_df()
+        else:
+            df_airline, df_preference, df_time = dfs
+        self.sky = Sky(df_time, timestep = cfg["timestep"], debug=False)
+        self.airports = {icao:Airport( df_preference, icao, debug=False, **cfg) for icao in df_airline.values}
+        self.sky.add_random_flight(df_preference, n=num_plane)
+        
+    def step():
+        arrivals = sky.arrivals
+        flight_info_lst = []
+        for code, airport in airports.items():
+            if code in arrivals:
+                flight_info = airport.update( arrivals[code] )
+            else:
+                flight_info = airport.update( None )
+            if flight_info is not None:
+                flight_info_lst.append(flight_info)#( self.code, next_station, delay )
+        new_flights = sky.update(flight_info_lst, [i for v in arrivals.values() for i in v.index])
+    
+    
+if __name__ =="__main__":
+    ## Test
+    
+    airports, df_preference, df_time = get_df()
+    sky = Sky(df_time)
+    airport_RKSI = Airport( df_preference, "RKSI", debug=True)
+    airports = {"RKSI":airport_RKSI}
+
+    sky.update([("RKPC","RKSI",0),("RKPC","RKSI",10),("RKPC","RKSI",20)],[])
+
+    for i in range(60):
+        arrivals = sky.arrivals
+        for code, airport in airports.items():
+            if code in arrivals:
+                flight_info = airport.update( arrivals[code] )
+            else:
+                flight_info = airport.update( None )
+        ## sky.update(flight_info_lst, [i for v in arrivals.values() for i in v.index])
+        sky.update([], [i for v in arrivals.values() for i in v.index])
